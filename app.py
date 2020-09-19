0bdd1e6b3e23ad45dfb7e55b9e6dfac08125d3a9
Hilary Zen <hzen00@stuy.edu>
2020-09-19 00:38:03 -0400

    Got JSON on page

--- app.py
+++ app.py
@@ -5,11 +5,19 @@ from flask import session
 from flask import redirect
 from flask import flash
 from flask import url_for
+import json
+import urllib
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('homepage.html')
+
+@app.route('/wellesley')
+def wellesley():
+    pop = urllib.request.urlopen("https://api.webscraper.io/api/v1/scraping-job/2817859/json?api_token=kBwQqhHkuCA1zvQXc44plXxzi0wLo90HqTWAbV01xGCMMS8YiXI1TO2hpkCU")
+    data = [json.loads(pop.read())]
+    return render_template('wellesley.html', data=data)
 
 if __name__ == "__main__":
     app.debug = True
