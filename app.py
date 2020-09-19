f17e2fd52f2deee58e89cc2355b9f9c0f0bb2f4d
Hilary Zen <hzen00@stuy.edu>
2020-09-19 18:37:29 -0400

    Finished harvard data

--- app.py
+++ app.py
@@ -101,6 +101,7 @@ def get_mit_data():
     url = "https://api.webscraper.io/api/v1/scraping-job/" + str(scraping_job_id).strip() + "/json?api_token=kBwQqhHkuCA1zvQXc44plXxzi0wLo90HqTWAbV01xGCMMS8YiXI1TO2hpkCU"
     pop = requests.get(url)
     data = json.loads(pop.text)
+    # Adding daily totals for tests and positive tests
     tests_array = ['sat-tests', 'fri-tests', 'thurs-tests', 'wed-tests', 'tues-tests', 'mon-tests', 'sun-tests']
     tests_total = 0
     for test in tests_array:
@@ -109,20 +110,33 @@ def get_mit_data():
     pos_tests_total = 0
     for pos_test in pos_tests_array:
         pos_tests_total += float(str(data[pos_test]).replace(',', ''))
-    # tests = float(data['sat-tests']) + float(data['fri-tests']) + data['thurs-tests'] + data['wed-tests'] + data['tues-tests'] + data['mon-tests'] + data['sun-tests']
-    # pos_tests = data['sat-pos-tests'] + data['fri-pos-tests'] + data['thurs-pos-tests'] + data['wed-pos-tests'] + data['tues-pos-tests'] + data['mon-pos-tests'] + data['sun-pos-tests']
     results = ['MIT', int(tests_total), int(pos_tests_total)]
     return results # College name, weekly tests, weekly positive cases
 
 def get_harvard_data():
-    url = "https://www.harvard.edu/coronavirus/harvard-university-wide-covid-19-testing-dashboard"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    results = soup.find_all('div', class_='card__text')
-    data = ['Harvard']
-    data.append(results[0].text.strip()[:-1])
-    data.append(results[1].text.strip())
-    return data # College name, weekly tests, weekly positive cases
+    # url = "https://www.harvard.edu/coronavirus/harvard-university-wide-covid-19-testing-dashboard"
+    # page = requests.get(url)
+    # soup = BeautifulSoup(page.content, 'html.parser')
+    # results = soup.find_all('div', class_='card__text')
+    # data = ['Harvard']
+    # data.append(results[0].text.strip()[:-1])
+    # data.append(results[1].text.strip())
+    scraping_job_creation = {
+    	"sitemap_id": 331306,
+    	"driver": "fulljs",
+    	"page_load_delay": 2000,
+    	"request_interval": 2000,
+    	"proxy": 0
+    }
+    req = requests.post("https://api.webscraper.io/api/v1/scraping-job?api_token=kBwQqhHkuCA1zvQXc44plXxzi0wLo90HqTWAbV01xGCMMS8YiXI1TO2hpkCU", json=scraping_job_creation)
+    resp = json.loads(req.text)
+    scraping_job_id = int(resp['data']['id'])
+    time.sleep(30)
+    url = "https://api.webscraper.io/api/v1/scraping-job/" + str(scraping_job_id).strip() + "/json?api_token=kBwQqhHkuCA1zvQXc44plXxzi0wLo90HqTWAbV01xGCMMS8YiXI1TO2hpkCU"
+    pop = requests.get(url)
+    data = json.loads(pop.text)
+    results = ['Harvard', data['7-days-tests'], data['7-days-pos-tests']]
+    return results # College name, weekly tests, weekly positive cases
 
 def get_bu_data():
     return "hi"
