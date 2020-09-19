02773f5f2439b6afc89d9aab842ff71fe3e8692d
Hilary Zen <hzen00@stuy.edu>
2020-09-19 00:02:53 -0400

    Changed index

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask import url_for
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "Hello world!"
+    return render_template('homepage.html')
 
 if __name__ == "__main__":
     app.debug = True
